dbb8a08287211b36ed82a923e70a6d92364c873e
Arshdeep Singh <deepsingh13131212@gmail.com>
2025-05-14 02:39:25 +0530

    debugged

--- server.js
+++ server.js
@@ -25,12 +25,12 @@ app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/store', storeRoutes);
 
 // Serve static files in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('client/build'));
+//   app.get('*', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+//   });
+// }
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
